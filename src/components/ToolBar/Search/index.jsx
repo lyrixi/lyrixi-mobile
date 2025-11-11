7de5d9d15cfb096bbb1fb56421c53ecb615bc2b3
wangmingzhu <49201650@qq.com>
2025-11-11 13:27:38 +0800

    feat: optmize ToolBar

--- src/components/ToolBar/Search/index.jsx
+++ src/components/ToolBar/Search/index.jsx
@@ -12,20 +12,91 @@ import { DOMUtil, LocaleUtil, Input } from 'lyrixi-mobile'
 
 const Search = (
   {
+    id,
+    name,
+
+    // Value & Display Value
+    value = '',
     placeholder,
-    // 其它属性
+    formatter,
+
+    // Status
+    readOnly,
+    disabled,
+    allowClear,
+    autoFocus,
+    autoSelect,
+
+    // Style
+    style,
     className,
-    ...props
+
+    // Element
+    inputRender,
+    leftIcon,
+    rightIcon,
+    clearRender,
+
+    // Validate
+    precision, // 小数精度, 只有数值框才生效
+    trim, // [Number框]小数位补0, true: 不补0; false: 补0。 [Text框]影响左右空格;
+    max,
+    min,
+    maxLength,
+
+    // input属性
+    inputMode,
+
+    // Events
+    onClick,
+    onChange,
+    onBlur,
+    onFocus,
+    onKeyDown,
+    onPressEnter,
+    onSearch
   },
   ref
 ) => {
   return (
     <Input.Search
       ref={ref}
-      leftIcon={<i className="lyrixi-toolbar-search-input-left-icon" />}
-      {...props}
-      className={DOMUtil.classNames('lyrixi-toolbar-search-input', className)}
+      id={id}
+      name={name}
+      // Value & Display Value
+      value={value}
       placeholder={placeholder || LocaleUtil.locale('搜索', 'lyrixi_search')}
+      formatter={formatter}
+      // Status
+      readOnly={readOnly}
+      disabled={disabled}
+      allowClear={allowClear}
+      autoFocus={autoFocus}
+      autoSelect={autoSelect}
+      // Style
+      style={style}
+      className={DOMUtil.classNames('lyrixi-toolbar-search-input', className)}
+      // Element
+      inputRender={inputRender}
+      leftIcon={<i className="lyrixi-toolbar-search-input-left-icon" />}
+      rightIcon={rightIcon}
+      clearRender={clearRender}
+      // Validate
+      precision={precision}
+      trim={trim}
+      max={max}
+      min={min}
+      maxLength={maxLength}
+      // input属性
+      inputMode={inputMode}
+      // Events
+      onClick={onClick}
+      onChange={onChange}
+      onBlur={onBlur}
+      onFocus={onFocus}
+      onKeyDown={onKeyDown}
+      onPressEnter={onPressEnter}
+      onSearch={onSearch}
     />
   )
 }
--- src/components/ToolBar/SearchActive/index.jsx
+++ src/components/ToolBar/SearchActive/index.jsx
@@ -11,16 +11,96 @@ import LocaleUtil from './../../../utils/LocaleUtil'
 import { DOMUtil, LocaleUtil } from 'lyrixi-mobile'
 测试使用-end */
 
-const SearchBar = ({ className, style, onCancel, searchProps, ...props }, ref) => {
+const SearchBar = (
+  {
+    id,
+    name,
+
+    // Value & Display Value
+    value = '',
+    placeholder,
+    formatter,
+
+    // Status
+    readOnly,
+    disabled,
+    allowClear,
+    autoFocus,
+    autoSelect,
+
+    // Style
+    style,
+    className,
+
+    // Element
+    inputRender,
+    leftIcon,
+    rightIcon,
+    clearRender,
+
+    // Validate
+    precision, // 小数精度, 只有数值框才生效
+    trim, // [Number框]小数位补0, true: 不补0; false: 补0。 [Text框]影响左右空格;
+    max,
+    min,
+    maxLength,
+
+    // input属性
+    inputMode,
+
+    // Events
+    onClick,
+    onChange,
+    onBlur,
+    onFocus,
+    onKeyDown,
+    onPressEnter,
+    onSearch,
+    onCancel
+  },
+  ref
+) => {
   return (
     <div className={DOMUtil.classNames('lyrixi-toolbar-search-bar', className)} style={style}>
       {/* 文本框 */}
       <Search
         ref={ref}
-        autoFocus
-        {...props}
-        {...searchProps}
-        className={DOMUtil.classNames('lyrixi-active', searchProps?.className)}
+        id={id}
+        name={name}
+        // Value & Display Value
+        value={value}
+        placeholder={placeholder || LocaleUtil.locale('搜索', 'lyrixi_search')}
+        formatter={formatter}
+        // Status
+        readOnly={readOnly}
+        disabled={disabled}
+        allowClear={allowClear}
+        autoFocus={true}
+        autoSelect={autoSelect}
+        // Style
+        style={style}
+        className={DOMUtil.classNames('lyrixi-active', className)}
+        // Element
+        inputRender={inputRender}
+        leftIcon={leftIcon || <i className="lyrixi-toolbar-search-input-left-icon" />}
+        rightIcon={rightIcon}
+        clearRender={clearRender}
+        // Validate
+        precision={precision}
+        trim={trim}
+        max={max}
+        min={min}
+        maxLength={maxLength}
+        // input属性
+        inputMode={inputMode}
+        // Events
+        onClick={onClick}
+        onChange={onChange}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        onKeyDown={onKeyDown}
+        onPressEnter={onPressEnter}
+        onSearch={onSearch}
       />
 
       {/* 取消按钮 */}
