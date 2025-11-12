9528a0242e6b110e83018ca8ba8b0e5d2a321694
wangmingzhu <49201650@qq.com>
2025-11-12 14:55:45 +0800

    feat: develop Bridge

--- src/utils/Bridge/index.js
+++ src/utils/Bridge/index.js
@@ -1,7 +1,39 @@
-import Bridge from './Bridge.js'
 import back from './utils/back'
 import formatOpenLocationParams from './utils/formatOpenLocationParams'
 
+import BridgeBrowser from './browser'
+import BridgeWx from './wx'
+import BridgeAlipay from './ap'
+import BridgeDingtalk from './dingtalk'
+import BridgeLark from './lark'
+
+// 内库使用-start
+import Device from './../Device'
+// 内库使用-end
+
+/* 测试使用-start
+import { Device } from 'lyrixi-mobile'
+测试使用-end */
+
+let Bridge = {}
+if (
+  Device.platform === 'wechat' ||
+  Device.platform === 'wecom' ||
+  Device.platform === 'wechatMiniprogram' ||
+  Device.platform === 'wecomMiniprogram'
+) {
+  Bridge = BridgeWx
+} else if (Device.platform === 'alipay' || Device.platform === 'alipayMiniprogram') {
+  Bridge = BridgeAlipay
+} else if (Device.platform === 'dingtalk') {
+  Bridge = BridgeDingtalk
+} else if (Device.platform === 'lark') {
+  Bridge = BridgeLark
+} else {
+  Bridge = BridgeBrowser
+}
+
+// Expose 工具类
 Bridge.utils = {
   back: back,
   formatOpenLocationParams: formatOpenLocationParams
