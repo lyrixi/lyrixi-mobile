9efbfc9764fcb415d8d2664f0294c10c775e6b52
wangmingzhu <49201650@qq.com>
2025-12-01 15:27:30 +0800

    feat: develop fileChoose

--- src/components/Attach/utils/fileChoose/index.js
+++ src/components/Attach/utils/fileChoose/index.js
@@ -99,6 +99,8 @@ async function fileChoose({
   }
 
   if (!Array.isArray(currentList) || _.isEmpty(currentList)) {
+    console.error('onFileChange返回的数据不正确, 请返回数组', currentList)
+    onChange && onChange(null, { action: 'choose' })
     return null
   }
 
