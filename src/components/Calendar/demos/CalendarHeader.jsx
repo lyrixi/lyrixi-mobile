6a7e407ca4f72aaab3bb16c518aa3455b2db4bee
wangmingzhu <49201650@qq.com>
2026-02-05 20:15:55 +0800

    feat: develop Calendar.Header

--- src/components/Calendar/demos/CalendarHeader.jsx
+++ src/components/Calendar/demos/CalendarHeader.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import { Page, Calendar } from 'lyrixi-mobile'
+import React, { useState } from 'react'
+import { Page, Calendar, DateUtil } from 'lyrixi-mobile'
 
 export default () => {
+  const [value, setValue] = useState(null)
+
   return (
     <Page>
       <Page.Main>
-        <Calendar.Header>
-          {/* Calendar.Header 示例内容 */}
-        </Calendar.Header>
+        <Calendar
+          value={value}
+          onChange={setValue}
+          headerRender={({ drawDate, onPreviousMonth, onNextMonth, onPreviousYear, onNextYear }) => (
+            <Calendar.Header
+              onPreviousMonth={onPreviousMonth}
+              onNextMonth={onNextMonth}
+              onPreviousYear={onPreviousYear}
+              onNextYear={onNextYear}
+            >
+              {drawDate ? DateUtil.format(drawDate, 'YYYY年MM月') : ''}
+            </Calendar.Header>
+          )}
+        />
       </Page.Main>
     </Page>
   )
