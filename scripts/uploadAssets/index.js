bf28903d8f53e082c965fe5a676ad416a6190137
wangmingzhu <49201650@qq.com>
2026-02-10 19:08:03 +0800

    feat: develop uploadAssets

--- scripts/uploadAssets/index.js
+++ scripts/uploadAssets/index.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const assets = path.resolve(__dirname, './assets')
-const serverPathPrefix = 'p/qince/h5/common/'
+const serverPathPrefix = 'serverPath/'
 
 // 上传到阿里云 OSS
 const upload = require('./upload')
