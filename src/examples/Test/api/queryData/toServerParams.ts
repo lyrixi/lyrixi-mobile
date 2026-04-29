4f590750e76bb28b047215c4ce899eadab7b71d5
wangmingzhu <49201650@qq.com>
2026-04-29 15:36:30 +0800

    feat: delete test

--- src/examples/Test/api/queryData/toServerParams.ts
+++
@@ -1,8 +0,0 @@
-/**
- * queryParams 转换为服务端参数
- */
-function toServerParams(queryParams: Record<string, unknown>): Record<string, unknown> {
-  return { keyword: queryParams?.keyword }
-}
-
-export default toServerParams
