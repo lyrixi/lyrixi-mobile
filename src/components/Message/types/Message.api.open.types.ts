f94f12c018ffca93ef1d127e600b4942b605ffe3
wangmingzhu <49201650@qq.com>
2026-06-04 14:20:52 +0800

    feat: develop Message

--- src/components/Message/types/Message.api.open.types.ts
+++ src/components/Message/types/Message.api.open.types.ts
@@ -1,6 +1,6 @@
 import type { MessageComboButton, MessageComboProps } from './Message.Combo.types'
 
-export interface MessageOpenButton extends MessageComboButton {}
+export type MessageOpenButton = MessageComboButton
 
 export interface MessageOpenProps extends Omit<MessageComboProps, 'buttons'> {
   buttons?: MessageOpenButton[]
