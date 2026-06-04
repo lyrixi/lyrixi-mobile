6b46bfeaa8b094b35aa570d12beaa5f35b246f6b
wangmingzhu <49201650@qq.com>
2026-06-04 17:18:07 +0800

    feat: develop ReactDOMClientCompat

--- src/utils/ReactDOMClientCompat/index.ts
+++ src/utils/ReactDOMClientCompat/index.ts
@@ -6,18 +6,19 @@ import type { ReactDOMClientCompatRoot } from './types'
 
 export type { ReactDOMClientCompatRoot } from './types'
 
-function isReact18OrNewer(): boolean {
-  return parseInt(React.version.split('.')[0] ?? '0', 10) >= 18
+// 判断版本
+function isReactBelow18(): boolean {
+  return parseInt(React.version.split('.')[0] ?? '0', 10) < 18
 }
 
+// 18创建
 function createReact18Root(container: Element): ReactDOMClientCompatRoot {
   // eslint-disable-next-line @typescript-eslint/no-require-imports, global-require
-  const { createRoot } = require('react-dom/client') as {
-    createRoot: (container: Element) => ReactDOMClientCompatRoot
-  }
+  const { createRoot } = require('react-dom/client')
   return createRoot(container)
 }
 
+// 17创建
 function createReact17Root(container: Element): ReactDOMClientCompatRoot {
   const render = (ReactDOM as { render?: (element: ReactNode, container: Element) => void }).render
   const unmount = (ReactDOM as { unmountComponentAtNode?: (container: Element) => boolean })
@@ -38,5 +39,5 @@ function createReact17Root(container: Element): ReactDOMClientCompatRoot {
 
 /** createRoot：18+ 用 react-dom/client，17 用 render 垫片（供 Message.open） */
 export function createRoot(container: Element): ReactDOMClientCompatRoot {
-  return isReact18OrNewer() ? createReact18Root(container) : createReact17Root(container)
+  return isReactBelow18() ? createReact17Root(container) : createReact18Root(container)
 }
