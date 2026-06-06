4dc6266516c3992b23a36bf3d6d07f5f92196b9d
wangmingzhu <49201650@qq.com>
2026-06-06 17:18:08 +0800

    feat: Switch

--- src/components/Switch/demos/Switch.tsx
+++ src/components/Switch/demos/Switch.tsx
@@ -6,37 +6,33 @@ export default function SwitchDemo() {
   return (
     <Page>
       <Page.Main>
-        <Divider>Size m</Divider>
-        <Switch
-          // disabled
-          checked={checked}
-          onChange={setChecked}
-        />
-        <Switch
-          // disabled
-          checked={checked}
-          on="On On On On"
-          off="Off"
-          onChange={setChecked}
-        />
+        <Divider>Size xxxs</Divider>
+        <Switch size="xxxs" checked={checked} onChange={setChecked} />
+        <Switch size="xxxs" checked={checked} on="On On On On" off="Off" onChange={setChecked} />
+        <Divider>Size xxs</Divider>
+        <Switch size="xxs" checked={checked} onChange={setChecked} />
+        <Switch size="xxs" checked={checked} on="On On On On" off="Off" onChange={setChecked} />
+        <Divider>Size xs</Divider>
+        <Switch size="xs" checked={checked} onChange={setChecked} />
+        <Switch size="xs" checked={checked} on="On On On On" off="Off" onChange={setChecked} />
         <Divider>Size s</Divider>
-        <Switch
-          size="s"
-          // disabled
-          checked={checked}
-          onChange={setChecked}
-        />
-        <Switch
-          size="s"
-          // disabled
-          checked={checked}
-          on="On On On On"
-          off="Off"
-          onChange={setChecked}
-        />
+        <Switch size="s" checked={checked} onChange={setChecked} />
+        <Switch size="s" checked={checked} on="On On On On" off="Off" onChange={setChecked} />
+        <Divider>Size m</Divider>
+        <Switch checked={checked} onChange={setChecked} />
+        <Switch checked={checked} on="On On On On" off="Off" onChange={setChecked} />
         <Divider>Size l</Divider>
         <Switch size="l" checked={checked} onChange={setChecked} />
         <Switch size="l" checked={checked} on="On On On On" off="Off" onChange={setChecked} />
+        <Divider>Size xl</Divider>
+        <Switch size="xl" checked={checked} onChange={setChecked} />
+        <Switch size="xl" checked={checked} on="On On On On" off="Off" onChange={setChecked} />
+        <Divider>Size xxl</Divider>
+        <Switch size="xxl" checked={checked} onChange={setChecked} />
+        <Switch size="xxl" checked={checked} on="On On On On" off="Off" onChange={setChecked} />
+        <Divider>Size xxxl</Divider>
+        <Switch size="xxxl" checked={checked} onChange={setChecked} />
+        <Switch size="xxxl" checked={checked} on="On On On On" off="Off" onChange={setChecked} />
       </Page.Main>
     </Page>
   )
