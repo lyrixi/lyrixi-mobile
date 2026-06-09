58ada69dc49a069832b2f158fdeefa480910353c
wangmingzhu <49201650@qq.com>
2026-06-09 10:31:26 +0800

    feat: FilterFooter

--- src/components/ToolBar/demos/ToolBarFilter.tsx
+++ src/components/ToolBar/demos/ToolBarFilter.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Page, ToolBar, FooterBar, Icons, Button } from 'lyrixi-mobile'
+import { Page, ToolBar } from 'lyrixi-mobile'
+import FilterFooter from './FilterFooter'
 
 export default function ToolBarFilterDemo() {
   return (
@@ -10,67 +11,7 @@ export default function ToolBarFilterDemo() {
             return <div>Modal Content</div>
           }}
           footerRender={({ onClose }) => {
-            return (
-              <FooterBar>
-                <FooterBar.Button
-                  maskStyle={{ zIndex: 12 }}
-                  direction="vertical"
-                  fontSize="12px"
-                  list={[
-                    {
-                      id: 'option1',
-                      name: 'Option 1',
-                      onClick: (e) => {
-                        console.log(e)
-                      }
-                    },
-                    {
-                      id: 'option2',
-                      name: 'Option 2',
-                      onClick: (e) => {
-                        console.log(e)
-                      }
-                    }
-                  ]}
-                  style={{ fontSize: '12px', flex: 'none', padding: '0 12px' }}
-                >
-                  <Button.Icon svg={Icons.ThreeDots} size="xxxs" />
-                  <Button.Text>More</Button.Text>
-                </FooterBar.Button>
-                <FooterBar.Button
-                  direction="vertical"
-                  fontSize="12px"
-                  onClick={(e) => {
-                    console.log(e)
-                  }}
-                  style={{ fontSize: '12px', flex: 'none', padding: '0 12px' }}
-                >
-                  <Button.Icon svg={Icons.Config} />
-                  <Button.Text>Config</Button.Text>
-                </FooterBar.Button>
-                <FooterBar.Button
-                  block
-                  variant="filled"
-                  color="default"
-                  onClick={(e) => {
-                    console.log(e)
-                  }}
-                >
-                  Cancel
-                </FooterBar.Button>
-                <FooterBar.Button
-                  block
-                  variant="solid"
-                  color="primary"
-                  onClick={(e) => {
-                    console.log(e)
-                    onClose?.()
-                  }}
-                >
-                  Ok
-                </FooterBar.Button>
-              </FooterBar>
-            )
+            return <FilterFooter onClose={onClose} />
           }}
         />
       </Page.Main>
