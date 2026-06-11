e596bd296625703acc9a967d9605ad537ce0507b
wangmingzhu <49201650@qq.com>
2026-06-11 09:49:07 +0800

    feat: develop svg

--- src/svg-modules.d.ts
+++ src/svg-modules.d.ts
@@ -1,6 +1,7 @@
 declare module '*.svg' {
   import type { FC, SVGProps } from 'react'
 
+  export const ReactComponent: FC<SVGProps<SVGSVGElement>>
   const SvgComponent: FC<SVGProps<SVGSVGElement>>
   export default SvgComponent
 }
