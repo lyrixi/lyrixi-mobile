d6c6059ac89b3831ab0456ad902248923e2f6cce
wangmingzhu <49201650@qq.com>
2026-06-11 17:48:11 +0800

    Checkbox

--- src/components/Checkbox/Checkbox/Icon.tsx
+++ src/components/Checkbox/Checkbox/Icon.tsx
@@ -13,7 +13,7 @@ import { DOMUtil, Icon as LyrixiIcon, Icons } from 'lyrixi-mobile'
 测试使用-end */
 
 function getCheckIconSize(variant: CheckboxIconProps['variant']): string {
-  return variant === 'text' ? 'm' : 'xxxs'
+  return variant === 'text' ? 'xs' : 'xxxs'
 }
 
 function getCheckIconColor(variant: CheckboxIconProps['variant']): string | undefined {
