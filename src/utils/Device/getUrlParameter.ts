f544ccc75045b28e6903082be95620ba61aa05c3
wangmingzhu <49201650@qq.com>
2026-06-30 19:43:56 +0800

    feat: getUrlParameter

--- src/utils/Device/getUrlParameter.ts
+++ src/utils/Device/getUrlParameter.ts
@@ -1,16 +1,15 @@
 /**
  * 获取地址栏参数
- * @param {String} argName - 参数名，如果不传则返回所有参数对象或查询字符串
- * @param {String} argSearch - 自定义查询字符串，如果不传则使用 window.location.href
+ * @param {String} paramName - 参数名，如果不传则返回所有参数对象或查询字符串
+ * @param {String} url - 自定义查询字符串，如果不传则使用 window.location.href
  * @returns {String|Object} 参数值、所有参数对象或查询字符串
  */
-function getUrlParameter(argName?: string, argSearch?: string) {
-  let url = window.location.href
-  if (argSearch) url = argSearch
+function getUrlParameter(paramName?: string, url?: string) {
+  // eslint-disable-next-line no-param-reassign
+  if (!url) url = window.location.href
   const params: Record<string, string> = {}
   // 如果url中包含?说明有参数
   if (url?.indexOf?.('?') !== -1) {
-    if (!argName) return '?' + url.split('?')[1]
     // 获取所有参数options: 如?a=1&b=2转为['a=1','b=2']
     let options = url.split('?')[1]?.split('&')
     if (options?.length) {
@@ -18,18 +17,26 @@ function getUrlParameter(argName?: string, argSearch?: string) {
         // 获取单项option: 如'a=1'转为['a', '1']
         let option = options[i].split('=')
         if (option.length === 2) {
-          if (argName) {
-            if (argName === option[0]) return option[1]
-          } else {
-            params[option[0]] = option[1]
-          }
+          params[option[0]] = option[1]
         }
       }
     }
   }
-  if (Object.keys(params).length) return params
-  return ''
+
+  // 无参时, 指定参数名返回空字符串, 否则返回null
+  if (!Object.keys(params).length) {
+    if (paramName) return ''
+    return null
+  }
+
+  // 有参时, 指定参数名字符串, 否则返回全部参数
+  if (paramName) {
+    if (params.hasOwnProperty(paramName)) {
+      return params[paramName as keyof typeof params]
+    }
+    return ''
+  }
+  return params
 }
 
 export default getUrlParameter
-
